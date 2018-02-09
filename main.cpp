e18de6664b23ca645af6fdbeda7060274c65db8d
mkalmheiri <mkalmheiri@mail.csuchico.edu>
2018-02-08 18:19:03 -0800

    Find type of letter

--- main.cpp
+++ main.cpp
@@ -6,19 +6,33 @@ using namespace std;
 int main()
 {
 
+char L;
+
+
   cout<<"What character do you want to know about?\n";
 
+cin>>L;
+
+if (L>='A' && L<='Z'){
   //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
+  cout<<L<<" is an upper case letter!\n";
+}
 
+
+else if(L>='a' && L<='z'){
   //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
+  cout<<L<<" is a lower case letter!\n";
+}
 
+
+else{
   //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
-  
+  cout<<L<<"?! Pssh. What are you talking about?\n";
+  }
+
+
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  cout<<"The ASCII value is: "<<(int)L<<endl;
 
   return 0;
 }
